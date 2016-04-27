e5f0427e2db386cca72d127eb5dd1bbf422da10c
Erick-Munene <munenewahome@gmail.com>
2016-04-27 23:19:01 +0300

    changed the navigation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation()
+
+(function () {
+    var toggle = document.getElementById('header-nav-toggle'),
+        nav = document.getElementById('header-nav');
+
+    // If JavaScript Is Enabled
+    // Modify Initial States
+    toggle.className = 'is-visible';
+    nav.className = 'is-hidden';
+
+    // Change Visibility On Click
+    toggle.addEventListener('click', function () {
+        if (nav.className === 'is-hidden') {
+            nav.className = '';
+        } else {
+            nav.className = 'is-hidden';
+        }
+    })
+})();
