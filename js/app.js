2c4891ff7b966eb2e2c80c2602a098f0122c7391
Erick-Munene <munenewahome@gmail.com>
2016-05-13 19:46:09 +0300

    added new nav

--- js/app.js
+++ js/app.js
@@ -18,3 +18,22 @@ $(document).foundation()
         }
     })
 })();
+
+
+$(function() {
+
+  var stickyNav = $('#stickyNav'),
+      offset = stickyNav.offset(),
+      spacer = stickyNav.height();
+  
+  $(window).scroll(function() {
+                   
+    if($(this).scrollTop() >= offset.top + 1 && $('#stickyNav').not('fixed')) {
+      stickyNav.addClass('fixed'),
+      $('#spacer').height(spacer);
+    }else if ($(this).scrollTop() <= offset.top && stickyNav.hasClass('fixed')){
+      stickyNav.removeClass('fixed'),
+      $('#spacer').height(0);
+    }
+  });
+});
